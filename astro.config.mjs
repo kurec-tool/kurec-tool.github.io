67cb3c8b71ed1415fb7147304f4f75dd4e88f618
KOUNOIKE Yuusuke <yusuke.konoike@starley.jp>
2024-11-29 04:33:07 +0900

    mdxでmermaid sequence diagram書いてみる

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import expressiveCode from 'astro-expressive-code';
+import remarkMermaid from 'remark-mermaidjs'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [expressiveCode(), mdx()],
+  markdown: {
+    syntaxHighlight: "prism",
+    remarkPlugins: [remarkMermaid]
+  }
 });
